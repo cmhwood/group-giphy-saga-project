66abc192500ec08bc216b673d4d1fc3d9b987f8f
Abdi <abdullahia0215@gmail.com>
2024-06-07 11:46:38 -0500

    new results load each time you search

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -3,17 +3,23 @@ const axios = require("axios");
 const pool = require("../modules/pool");
 const router = express.Router();
 
+let offsetVariable = 0;
+
 router.get("/", (req, res) => {
   // console.log('this is the key recieved:',GIPHY_API_KEY)
   const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
-  const searchQuery = req.query.search
-  console.log(searchQuery)
+  const searchQuery = req.query.search;
+
+  console.log(searchQuery);
   axios
     .get(
-      `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${searchQuery}&limit=10`
+      `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${searchQuery}&limit=10&offset=${
+        offsetVariable + 10
+      }`
     )
     .then((response) => {
       res.send(response.data.data);
+      offsetVariable += 10;
     })
     .catch((error) => {
       res.sendStatus(500);
