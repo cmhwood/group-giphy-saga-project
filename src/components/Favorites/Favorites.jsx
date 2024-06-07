15ec53c0e45c7a110d9c3cf5dd75317e0ec025c3
Chris Harwood <cmharwood79@gmail.com>
2024-06-07 16:20:08 -0500

    updated dropdown on favorites page

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,28 +1,28 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import "../Favorites/Favorites.css";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import '../Favorites/Favorites.css';
 
 export default function FavoriteGifs() {
   const favorites = useSelector((store) => store.favorites);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch({ type: "FETCH_FAVORITES" });
+    dispatch({ type: 'FETCH_FAVORITES' });
   }, []);
 
   const updateCategory = (favID) => {
     const newCategory = Number(event.target.value);
     console.log(event.target.value);
-    console.log("category:", category);
-    console.log("id:", favID);
+    console.log('category:', category);
+    console.log('id:', favID);
     dispatchUpdate(newCategory, favID);
-    console.log("checking newCategory", newCategory.name);
+    console.log('checking newCategory', newCategory.name);
   };
 
   const dispatchUpdate = (category, favID) => {
     dispatch({
-      type: "UPDATE_CATEGORY",
+      type: 'UPDATE_CATEGORY',
       payload: {
         category_id: category,
         gifID: favID,
@@ -32,7 +32,7 @@ export default function FavoriteGifs() {
 
   const deleteFavorite = (favoriteID) => {
     dispatch({
-      type: "DELETE_FAVORITE",
+      type: 'DELETE_FAVORITE',
       payload: favoriteID,
     });
     // console.log('clicked:', favoriteID)
@@ -41,16 +41,12 @@ export default function FavoriteGifs() {
   return (
     <>
       <h1>Favorite Gifs</h1>
-      <div className="card-wrapper">  
-      {favorites.map((favorite) => (
-        <div
-          key={favorite.id}
-          className={"card page-card"}
-          style={{ width: "18rem" }}
-        >
-          <div className={"card-body"}>
-            <img src={favorite.url} className={"card-img-top"} />
-            {/* <p>
+      <div className='card-wrapper'>
+        {favorites.map((favorite) => (
+          <div key={favorite.id} className={'card page-card'} style={{ width: '18rem' }}>
+            <div className={'card-body'}>
+              <img src={favorite.url} className={'card-img-top'} />
+              {/* <p>
               Category:{" "}
               {favorite.category_id === 1
                 ? "Wild"
@@ -64,37 +60,36 @@ export default function FavoriteGifs() {
                 ? "Whimsical"
                 : "Add Category"}
             </p> */}
-            <p>Category: {favorite.category_name ? favorite.category_name : 'Add Category'}</p>
-            <button
-              onClick={() => {
-                deleteFavorite(favorite.id);
-              }} 
-              className="btn btn-primary"
-            >
-              Remove
-            </button>
-            <select
-              value={favorite.category_id}
-              name="category"
-              id="category"
-              onChange={(event) => {
-                const newCategory = Number(event.target.value);
-                updateCategory(favorite.id, newCategory);
-              }}
-            >
-              <option defaultValue>------</option>
+              <p>Category: {favorite.category_name ? favorite.category_name : 'Add Category'}</p>
+              <button
+                onClick={() => {
+                  deleteFavorite(favorite.id);
+                }}
+                className='btn btn-primary'
+              >
+                Remove
+              </button>
+              <select
+                value={favorite.category_id}
+                name='category'
+                id='category'
+                onChange={(event) => {
+                  const newCategory = Number(event.target.value);
+                  updateCategory(favorite.id, newCategory);
+                }}
+              >
+                <option defaultValue>Select Category</option>
 
-              <option value="1">Wild</option>
-              <option value="2">Uproarious</option>
-              <option value="3">Poignant</option>
-              <option value="4">Felicitous</option>
-              <option value="5">Whimsical</option>
-            </select>
-            
+                <option value='1'>Wild</option>
+                <option value='2'>Uproarious</option>
+                <option value='3'>Poignant</option>
+                <option value='4'>Felicitous</option>
+                <option value='5'>Whimsical</option>
+              </select>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </>
   );
 }
